1c2adf0790a2a10714b9c1e622754753f9c3a570
YuHlushchenko <yuhlushchenko@gmail.com>
2025-10-22 13:37:59 +0300

    feat: form plungin

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,8 @@
-export default () => ({});
+export default () => ({
+    "api-forms": {
+        enabled: true,
+        config: {
+            // Add any specific configuration for the api-forms plugin here
+        }
+    }
+});
