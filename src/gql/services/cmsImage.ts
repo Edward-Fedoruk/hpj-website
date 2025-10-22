5feb739cd1f125db7f11e6bd6567b41ed236162e
YuHlushchenko <yuhlushchenko@gmail.com>
2025-10-22 11:20:30 +0300

    fix: img urls

--- src/gql/services/cmsImage.ts
+++ src/gql/services/cmsImage.ts
@@ -1,7 +1,7 @@
 export const CMS_URL = import.meta.env.PUBLIC_CMS_URL ?? "http://localhost:1337";
 
-export function abs(path?: string | null): string | null {
-  if (!path) return null;
-  
+export function abs(path?: string | null): string {
+  if (!path) return "";
+
   return path.startsWith("http") ? path : `${CMS_URL}${path}`;
 }
