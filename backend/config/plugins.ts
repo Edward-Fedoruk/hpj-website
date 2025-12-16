ad82a5b865475ca94d7fd151371fc0d5510f8b8c
Eduard Fedoruk <ed.fedorukk@gmail.com>
2025-12-16 12:33:43 +0200

    disable breakpoints

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,20 +1,13 @@
 export default () => ({
-    upload: {
-        config: {
-            breakpoints: {
-                thumbnail: 245,
-                small: 500,
-                medium: 750,
-                large: 1000,
-                xlarge: 1440,
-                xxlarge: 1920,
-            },
-        },
+  upload: {
+    config: {
+      breakpoints: {},
     },
-    "api-forms": {
-        enabled: true,
-        config: {
-            // Add any specific configuration for the api-forms plugin here
-        }
-    }
+  },
+  "api-forms": {
+    enabled: true,
+    config: {
+      // Add any specific configuration for the api-forms plugin here
+    },
+  },
 });
